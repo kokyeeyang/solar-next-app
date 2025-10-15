b325e03ae637365f49ad3bff80abe10cafd7689e
Kok Yee Yang <yee_yang94@hotmail.com>
2025-10-16 00:42:23 +0800

    debug

--- server/etl/runAllETL.js
+++ server/etl/runAllETL.js
@@ -1,5 +1,6 @@
 // server/etl/runAllETL.js
-import { runCandidateCallsETL } from "./jobs/candidateCallsEtl.js";
+// import { runCandidateCallsETL } from "./jobs/candidateCallsEtl.js";
+import { runCandidateCallsETL } from "./jobs/candidateCallsETL.js";
 // import { runCvsSentETL } from "./jobs/cvssentETL.js";
 // import { runInterviewsETL } from "./jobs/interviewsETL.js";
 (async () => {
