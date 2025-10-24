85df9feda662c07803e645b0ad1770f5b9bbb716
Kok Yee Yang <yee_yang94@hotmail.com>
2025-10-24 16:29:35 +0800

    added jobsadded route

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import jobsToCvsSentRouter from "./routes/jobsToCvsSent.js";
 import unfilledAJobsRouter from "./routes/unfilledAJobs.js";
 import candidateCallsRouter from "./routes/candidateCalls.js";
 import candidatesAddedRouter from "./routes/candidatesAdded.js";
+import jobsAddedRouter from "./routes/candidatesAdded.js";
 // import { connectDB } from "./db/connection.js";
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use("/api/jobs-to-cvs-sent", jobsToCvsSentRouter);
 app.use("/api/unfilled-a-jobs", unfilledAJobsRouter);
 app.use("/api/candidatecalls", candidateCallsRouter);
 app.use("/api/candidatesadded", candidatesAddedRouter);
+app.use("/api/jobsadded", jobsAddedRouter);
 
 // Healthcheck
 app.get("/", (req, res) => res.send("Backend running 🚀"));
